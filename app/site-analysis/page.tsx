bb6b6567b9c3edcce3d9fad236259e1b99458412
JReal2 <dlwjdfuf99@naver.com>
2025-02-10 21:02:42 +0900

    fix: useEffect 미적용 수정

--- app/site-analysis/page.tsx
+++ app/site-analysis/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Layout from '../components/Layout'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
